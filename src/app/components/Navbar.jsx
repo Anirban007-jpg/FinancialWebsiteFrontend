f26cd912b56655b3fb7a75d567ebcf09e34be74d
Anirban <abanerjee763@gmail.com>
2024-05-04 13:40:37 +0530

    first commit

---
+++ src/app/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className="h-full flex items-center justify-between">
+      <div>1</div>
+      <div>2</div>
+    </div>
+  )
+}
+
+export default Navbar
