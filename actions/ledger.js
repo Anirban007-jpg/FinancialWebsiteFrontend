99dfb8a7581b665cbc1f51d1653664ab6327f80e
Anirban <abanerjee763@gmail.com>
2024-05-12 16:21:27 +0530

    Closes #5

---
+++ actions/ledger.js
@@ -0,0 +1,17 @@
+
+import fetch from 'isomorphic-fetch';
+
+export const createLedger = (ledger, token) => {
+    return fetch(`${process.env.NEXT_PUBLIC_DOMAIN}/createLedger`, {
+        method: 'POST',
+        headers: {
+            Accept: 'application/json',
+            Authorization: `Bearer ${token}`
+        },
+        body : JSON.stringify(ledger)
+    })
+        .then(response => {
+            return response.json();
+        })
+        .catch(err => console.log(err));
+};
